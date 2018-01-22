4cfee54fd4b119ffde22f8a57f41f97281a23cbb
Sheldon Lynn <sheldonlynn@outlook.com>
2018-01-21 16:19:51 -0800

    Created quote display for individual actor pages

--- application/views/actor.php
+++ application/views/actor.php
@@ -1,7 +1,15 @@
-<div class="row">
-    <div class="span4">
+<div class="text-center actor-display">
+    <div class="mx-auto">
         <a href="/welcome/show/{key}">
-            <img src="/pix/{who}.jpg" title="{who} plays {role}"/>
+            <img class="actor-mugshot" src="/pix/{who}.jpg" title="{who} plays {role}"/>
         </a>
     </div>
+
+    <h2 class="quote-what">
+        "{what}"
+    </h2>
+    <div class="quote-attribute">
+        - <span class="quote-actor">{who}</span> as <span class="quote-role">{role}</span>
+    </div>
+
 </div>
